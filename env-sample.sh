858aa87040a15087921e513c9f6c6e99493e7a4f
Dinesh Alapati <dine.alapati@gmail.com>
2017-11-20 18:48:41 -0500

    add env-sample.sh file

---
+++ env-sample.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export DRONE_HOST=""
+export DRONE_GITHUB_CLIENT=""
+export DRONE_GITHUB_SECRET=""
+export DRONE_SECRET=""
+export DRONE_ORGS=""
+export DRONE_ADMINS=""
+export DRONE_GITHUB_SCOPE="repo,repo:status,user:email,read:org"
+export DRONE_GITHUB_CONTEXT="CI/drone"
